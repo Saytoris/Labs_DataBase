310a2391e97f89303dbda3635071d19d23cc8e15
Saytoris <igarlaptev@gmail.com>
2026-06-11 17:25:50 +0300

    Додано Лабу 9

--- Lab_9/data.sql
+++ Lab_9/data.sql
@@ -1,22 +1,3 @@
-import redis
-
-# Підключення до локального контейнера Redis
-client = redis.Redis(host='localhost', port=6379, decode_responses=True)
-
-# 1. Додавання нових даних у потік (логіка XADD)
-stream_name = 'mystream'
-message_data = {'sensor-id': '9999', 'temperature': '22.5', 'status': 'active'}
-message_id = client.xadd(stream_name, message_data)
-print(f"Дані успішно додано до потоку. ID повідомлення: {message_id}")
-
-# 2. Зчитування нових повідомлень зі стріму (логіка XREAD)
-# Читаємо 2 останні повідомлення, починаючи з самого початку ('0')
-messages = client.xread({stream_name: '0'}, count=2)
-
-print("Отримані дані з потоку:")
-for stream, data_list in messages:
-    for message_id, data in data_list:
-        print(f"ID: {message_id} -> Значення: {data}")
 -- Таблиці вимірів
 CREATE TABLE products (id SERIAL PRIMARY KEY, product_name VARCHAR(100), category VARCHAR(50));
 CREATE TABLE regions (id SERIAL PRIMARY KEY, region_name VARCHAR(50), country VARCHAR(50));
